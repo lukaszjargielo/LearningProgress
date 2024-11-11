f5fa79ea72239638a475010a5b004ad213b5f3f4
lukaszjargielo <l.jargielo88@gmail.com>
2024-11-11 19:47:07 +0100

    Exercise 1 with Calculator class and overloaded add() method

---
+++ OOP/src/pl/futurejavacode/oopExercises/Main.java
@@ -0,0 +1,10 @@
+package pl.futurejavacode.oopExercises;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(1,2));
+        System.out.println(Calculator.add(2.0f, 3.0f));
+        System.out.println(Calculator.add(2.0,9.0));
+        System.out.println(Calculator.add(23l,35l));
+    }
+}
