cc150c672f2effe7538e621e61d340ea2d246581
lukaszjargielo <l.jargielo88@gmail.com>
2024-11-18 09:59:31 +0100

    Training with null

---
+++ Java Advanced/src/pl/futurejavacode/NULL/Users.java
@@ -0,0 +1,22 @@
+package pl.futurejavacode.NULL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+    private List<User> users = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public User getFirstMaleUser() {
+        for (User user: users) {
+            if(!user.getName().endsWith("a")) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
+
