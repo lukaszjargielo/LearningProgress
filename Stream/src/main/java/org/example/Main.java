92ce38f0f990bb3e0b9d018ed4b12125b7a64d83
lukaszjargielo <l.jargielo88@gmail.com>
2024-12-19 16:50:48 +0100

    New Record Country and stream methods to collecting data

--- Stream/src/main/java/org/example/Main.java
+++ Stream/src/main/java/org/example/Main.java
@@ -1,31 +1,40 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<User> users = prepareData();
 
-        List<String> flatMapList = users.stream()
+        List<String> listOfSkills = users.stream()
                 .map(User::skills)
-                .flatMap(list -> list.stream())
-                .distinct()
-                .sorted()
-                .toList();
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
 
-        flatMapList.forEach(System.out::println);
+        listOfSkills.forEach(System.out::println);
         System.out.println();
 
-        List<String> list2 = users.stream()
+        Set<String> setOfSkills = users.stream()
                 .map(User::skills)
-                .flatMap(list -> list.stream())
-                .distinct()
-                .sorted(Comparator.reverseOrder())
-                .toList();
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
 
-        list2.forEach(System.out::println);
+        setOfSkills.forEach(System.out::println);
+        System.out.println();
+
+        Country country1 = new Country("Poland", "Cracow");
+        Country country2 = new Country("Germany", "Berlin");
+        Country country3 = new Country("France", "Paris");
+        Country country4 = new Country("Poland", "Warsaw");
+
+        List<Country> countries = List.of(country1, country2, country3);
+
+        Map<String, String> mapOfCountries = countries.stream()
+                .collect(Collectors.toMap(Country::name, Country::capital, (OldValue, newValue) -> newValue));
+
+        System.out.println(mapOfCountries.get("Poland"));
+
     }
     private static List<User> prepareData() {
         List<User> users = new ArrayList<>();
