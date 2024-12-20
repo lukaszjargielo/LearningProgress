e831fdf58d0683f5ec5a5fb216b2a04ad3fca0f9
lukaszjargielo <l.jargielo88@gmail.com>
2024-12-20 10:16:05 +0100

    Stream statistics method

--- Stream/src/main/java/org/example/Main.java
+++ Stream/src/main/java/org/example/Main.java
@@ -7,29 +7,58 @@ public class Main {
     public static void main(String[] args) {
         List<User> users = prepareData();
 
-        List<String> listOfSkills = users.stream()
-                .map(User::skills)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+        Long numberOfStreamElements = users.stream()
+                .collect(Collectors.counting());
 
-        listOfSkills.forEach(System.out::println);
+        System.out.println(numberOfStreamElements);
         System.out.println();
 
-        Set<String> setOfSkills = users.stream()
-                .map(User::skills)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toSet());
-
-        setOfSkills.forEach(System.out::println);
+        long countedStreamElements = users.stream()
+                .count();
+        System.out.println(countedStreamElements);
         System.out.println();
 
-        String  collectedElements = users.stream()
-                .map(User::skills)
-                .flatMap(Collection::stream)
-                .collect(Collectors.joining(", "));
+        IntSummaryStatistics sumOfAge = users.stream()
+                .collect(Collectors.summarizingInt(User::age));
 
-        System.out.println(collectedElements);
+        System.out.println(sumOfAge);
+        System.out.println();
 
+        Integer totalAge = users.stream()
+                .collect(Collectors.summingInt(User::age));
+
+        System.out.println(totalAge);
+        System.out.println();
+
+        int summedAge = users.stream().mapToInt(User::age).sum();
+
+        System.out.println(summedAge);
+        System.out.println();
+
+        Double averageAge = users.stream()
+                .collect(Collectors.averagingInt(User::age));
+
+        System.out.println(averageAge);
+
+        Optional<Integer> maxAge = users.stream()
+                .map(User::age)
+                .collect(Collectors.maxBy(Comparator.naturalOrder() ));
+
+        System.out.println(maxAge);
+
+        Optional<Integer> maxAge2 = users.stream()
+                .map(User::age)
+                .max(Comparator.naturalOrder());
+        System.out.println(maxAge2);
+
+        IntSummaryStatistics statisticsData = users.stream()
+                .collect(Collectors.summarizingInt(User::age));
+        System.out.println(statisticsData);
+        System.out.println(statisticsData.getMax());
+        System.out.println(statisticsData.getMin());
+        System.out.println(statisticsData.getAverage());
+        System.out.println(statisticsData.getSum());
+        System.out.println(statisticsData.getCount());
     }
     private static List<User> prepareData() {
         List<User> users = new ArrayList<>();
