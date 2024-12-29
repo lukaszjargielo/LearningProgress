c7ce9fd32ca88277a835ca7d654078d30f360b79
lukaszjargielo <l.jargielo88@gmail.com>
2024-12-29 13:21:43 +0100

    First null workflow

--- Optional/src/main/java/org/example/Main.java
+++ Optional/src/main/java/org/example/Main.java
@@ -3,20 +3,33 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Drive drive = new Drive("SSD", 1);
+        Drive drive = new Drive("ssd", 1);
         Computer computer = new Computer(drive);
-
-        System.out.println(computer.getDrive().getName());
 
         Drive drive2 = new Drive(null, 2);
         Computer computer2 = new Computer(drive2);
 
-//        System.out.println(computer2.getDrive().getName().toUpperCase());
+        if(computer.getDrive().isPresent()) {
+            Drive drive1 = computer.getDrive().get();
 
-        if (computer2.getDrive() != null) {
-            if (computer2.getDrive().getName() != null) {
-                System.out.println(computer2.getDrive().getName().toUpperCase(q));
+            if (drive1.getName().isPresent()) {
+                String name = drive1.getName().get();
+                System.out.println(name.toUpperCase());
             }
         }
+
+        if (computer2.getDrive().isPresent()) {
+            Drive drive1 = computer2.getDrive().get();
+
+            if(drive1.getName().isPresent()) {
+                String name = drive1.getName().get();
+                System.out.println(name);
+            }
+        }
+
+
+
+
+
     }
 }
