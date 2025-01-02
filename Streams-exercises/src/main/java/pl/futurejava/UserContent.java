68dff8eeaa6795814f845f1ba8e93c36706eab7a
lukaszjargielo <l.jargielo88@gmail.com>
2025-01-02 10:32:59 +0100

    Moving a logic from Main class to a separate method in UserContent class

--- Streams-exercises/src/main/java/pl/futurejava/UserContent.java
+++ Streams-exercises/src/main/java/pl/futurejava/UserContent.java
@@ -1,6 +1,9 @@
 package pl.futurejava;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserContent {
     private String username;
@@ -29,5 +32,25 @@ public class UserContent {
 
     public int getCommentLikes() {
         return commentLikes;
+    }
+
+    public List<UserContent> parseDataFromList(List<String> listWithData) {
+        ArrayList<UserContent> users = new ArrayList<>();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        for (int i = 1; i < listWithData.size(); i++) {
+            String[] split = (listWithData.get(i)).split(";");
+            String username = split[0];
+            String comment = split[1];
+            String dateInString = split[2];
+            LocalDate date = LocalDate.parse(dateInString, dateTimeFormatter);
+            int likes = Integer.parseInt(split[3]);
+
+            UserContent userContent = new UserContent(username, comment, date, likes);
+            users.add(userContent);
+
+        }
+
+        return users;
     }
 }
