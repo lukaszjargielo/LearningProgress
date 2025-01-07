a4fc48045edd8bc00f381feae3042f5d347b7baf
lukaszjargielo <l.jargielo88@gmail.com>
2025-01-07 15:59:36 +0100

    Serialization to JSON file

---
+++ maven-first-project/src/main/java/pl/futurejava/Car.java
@@ -0,0 +1,47 @@
+package pl.futurejava;
+
+public class Car {
+    private String make;
+    private int doorNumber;
+    private boolean is4x4;
+    private double fuelConsumptionPer100km;
+
+    public Car(String make, int doorNumber, boolean is4x4, double fuelConsumptionPer100km) {
+        this.make = make;
+        this.doorNumber = doorNumber;
+        this.is4x4 = is4x4;
+        this.fuelConsumptionPer100km = fuelConsumptionPer100km;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getDoorNumber() {
+        return doorNumber;
+    }
+
+    public void setDoorNumber(int doorNumber) {
+        this.doorNumber = doorNumber;
+    }
+
+    public boolean isIs4x4() {
+        return is4x4;
+    }
+
+    public void setIs4x4(boolean is4x4) {
+        this.is4x4 = is4x4;
+    }
+
+    public double getFuelConsumptionPer100km() {
+        return fuelConsumptionPer100km;
+    }
+
+    public void setFuelConsumptionPer100km(double fuelConsumptionPer100km) {
+        this.fuelConsumptionPer100km = fuelConsumptionPer100km;
+    }
+}
--- maven-first-project/src/main/java/pl/futurejava/Main.java
+++ maven-first-project/src/main/java/pl/futurejava/Main.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -19,6 +22,17 @@ public class Main {
         for (User user : users ) {
             System.out.println(user.getName() + ", " + user.getAge());
         }
+
+        Car car1 = new Car("Audi", 5, true, 10.2);
+        Car car2 = new Car("VW", 3, false, 6.5);
+        Car car3 = new Car("Kia", 5, true, 12.7);
+        Car car4 = new Car("BMW", 3, false, 8.1);
+
+        List<Car> cars = new ArrayList<>();
+        cars = Arrays.asList(car1, car2, car3, car4);
+
+        mapper.writeValue(new File("src/main/resources/cars.json"), car1);
+        mapper.writeValue(new File("src/main/resources/carsList.json"), cars);
 
     }
 }
