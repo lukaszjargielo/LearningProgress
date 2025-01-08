b7594faece770be58ad56d87a6ebe8cc1a95654b
lukaszjargielo <l.jargielo88@gmail.com>
2025-01-08 13:19:31 +0100

    Guava and Joiner - solution to the problem of nulls in collections

--- Guava/src/main/java/pl/futurejava/Main.java
+++ Guava/src/main/java/pl/futurejava/Main.java
@@ -1,11 +1,10 @@
 package pl.futurejava;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -14,26 +13,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        countriesAndCapitals.put("Poland", "Warsaw");
+        /*countriesAndCapitals.put("Poland", "Warsaw");
         countriesAndCapitals.put("Germany", "Berlin");
         countriesAndCapitals.put("France", "Paris");
 
         System.out.println(countriesAndCapitals.get("Poland"));
-        System.out.println(getCountry("Paris"));
+        System.out.println(getCountry("Paris"));*/
 
-        guavaCountriesAndCapitals.put("Poland", "Warsaw");
+        /*guavaCountriesAndCapitals.put("Poland", "Warsaw");
         guavaCountriesAndCapitals.put("Germany", "Berlin");
         guavaCountriesAndCapitals.put("France", "Paris");
 
-        System.out.println(guavaCountriesAndCapitals.inverse().get("Berlin"));
+        System.out.println(guavaCountriesAndCapitals.inverse().get("Berlin"))*/;
+
+        List<String> names = new ArrayList<>();
+        names.add("Lucas");
+        names.add("Mark");
+        names.add(null);
+        names.add("Robert");
+
+        String joinedNames = String.join("; ", names);
+        System.out.println(joinedNames);
+
+        String joinedNamesGuava = Joiner.on("; ").skipNulls().join(names);
+        System.out.println(joinedNamesGuava);
+
+        String joinedWithDefault = Joiner.on("; ").useForNull("default value").join(names);
+        System.out.println(joinedWithDefault);
     }
 
-    private static String getCountry(String capital) {
+    /*private static String getCountry(String capital) {
         Set<String> countries = countriesAndCapitals.keySet();
         for (String country : countries) {
             if (capital.equals(countriesAndCapitals.get(country)))
                 return country;
         }
         return null;
-    }
+    }*/
 }
