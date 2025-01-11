2efb5f9e8aab02309ca90ff9965929a82dd17666
lukaszjargielo <l.jargielo88@gmail.com>
2025-01-11 13:37:06 +0100

    StringUtils isEmpty(), isBlank() and defaultString methods

--- apacheCommons/src/main/java/pl/futurejava/Main.java
+++ apacheCommons/src/main/java/pl/futurejava/Main.java
@@ -22,12 +22,32 @@ public class Main {
         String out = substitutor.replace(template);
         System.out.println(out);*/
 
-        String string = "this is chapter title";
+        /*String string = "this is chapter title";
         String capitalized1 = StringUtils.capitalize(string);
         System.out.println(capitalized1);
 
         String capitalized2 = WordUtils.capitalize(string);
-        System.out.println(capitalized2);
+        System.out.println(capitalized2);*/
 
+        //blank - null || "" || " " || "        "
+        //empty - null || ""
+
+        System.out.println(StringUtils.isBlank(null));
+        System.out.println(StringUtils.isBlank(""));
+        System.out.println(StringUtils.isBlank("   "));
+        System.out.println(StringUtils.isBlank("            "));
+
+        System.out.println();
+
+        System.out.println(StringUtils.isEmpty(null));
+        System.out.println(StringUtils.isEmpty(""));
+        System.out.println(StringUtils.isEmpty(" "));
+        System.out.println(StringUtils.isEmpty("        "));
+
+        System.out.println(StringUtils.defaultString(getName(), "default value"));
     }
+
+     private static String getName() {
+        return null;
+     }
 }
