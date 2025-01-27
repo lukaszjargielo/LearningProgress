a45f5ad5f1db1d0956e1f96b7698e9a5e41c9e37
lukaszjargielo <l.jargielo88@gmail.com>
2025-01-27 16:41:35 +0100

    Skeleton of application

---
+++ SpringBootAppRepeat/src/main/java/pl/futurejava/SpringBootAppRepeat/SpringBootAppRepeatApplication.java
@@ -0,0 +1,15 @@
+package pl.futurejava.SpringBootAppRepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppRepeatApplication {
+
+	public static void main(String[] args) {
+
+		
+		SpringApplication.run(SpringBootAppRepeatApplication.class, args);
+	}
+
+}
---
+++ SpringBootAppRepeat/src/test/java/pl/futurejava/SpringBootAppRepeat/SpringBootAppRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.futurejava.SpringBootAppRepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
