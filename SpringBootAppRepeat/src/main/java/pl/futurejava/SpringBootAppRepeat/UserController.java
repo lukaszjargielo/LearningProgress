012dd94be3c7ec0cc08f5f3edd7fcb3aad9f90c0
lukaszjargielo <l.jargielo88@gmail.com>
2025-01-30 08:39:46 +0100

    Added method to update user

--- SpringBootAppRepeat/src/main/java/pl/futurejava/SpringBootAppRepeat/UserController.java
+++ SpringBootAppRepeat/src/main/java/pl/futurejava/SpringBootAppRepeat/UserController.java
@@ -5,15 +5,14 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -22,45 +21,83 @@ public class UserController {
     @GetMapping("users")
     public ResponseEntity<List<User>> getAllUsers() throws IOException {
 
-        try (Reader input = new FileReader("SpringBootAppRepeat/src/main/resources/users.csv")) {
+        List<User> users = getUsersFromFile();
 
-            CSVFormat csvFormat = getCSVFormat();
-
-            try (CSVParser parser = csvFormat.parse(input)) {
-
-
-                List<User> users = parser.stream()
-                        .map(record -> {
-                            String name = record.get("name");
-                            int age = Integer.parseInt(record.get("age"));
-                            boolean isMale = Boolean.parseBoolean(record.get("isMale"));
-                            return new User(name, age, isMale);
-                        }).toList();
-
-                return ResponseEntity.ok(users);
-
-            }
-        }
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping("users")
     public ResponseEntity<User> addUser(@RequestBody User user) throws IOException {
 
-        CSVFormat csvFormat = getCSVFormat();
+        List<User> users = getUsersFromFile();
+        int lastId = users.getLast().id();
+
+        CSVFormat csvFormat = getCSVFormat(true);
+
+        User newUser = new User(lastId + 1, user.name(), user.age(), user.isMale());
 
         try (CSVPrinter printer = new CSVPrinter(new FileWriter("SpringBootAppRepeat/src/main/resources/users.csv", true), csvFormat)) {
 
-            printer.printRecord(user.name(), user.age(), user.isMale());
+            printer.printRecord(newUser.id(), newUser.name(), newUser.age(), newUser.isMale());
         }
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
-    private CSVFormat getCSVFormat() {
+    @PutMapping("users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) throws IOException {
+
+        List<User> usersFromFile = getUsersFromFile();
+        List<User> updatedUsers = new ArrayList<>();
+        User updatedUser = null;
+
+        for (User userFromFile : usersFromFile) {
+            if (userFromFile.id() == user.id()) {
+                updatedUser = new User(user.id(), user.name(), user.age(), user.isMale());
+                updatedUsers.add(updatedUser);
+            } else {
+                updatedUsers.add(userFromFile);
+            }
+        }
+
+        CSVFormat csvFormat = getCSVFormat(false);
+
+        try (CSVPrinter printer = new CSVPrinter(new FileWriter("SpringBootAppRepeat/src/main/resources/users.csv"), csvFormat)) {
+            for (User newUser : updatedUsers) {
+                printer.printRecord(newUser.id(), newUser.name(), newUser.age(), newUser.isMale());
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
+    }
+
+    private CSVFormat getCSVFormat(boolean setSkipHeaderRecord) {
 
         return CSVFormat.DEFAULT.builder()
-                .setHeader("name", "age", "isMale")
-                .setSkipHeaderRecord(true)
+                .setHeader("id", "name", "age", "isMale")
+                .setSkipHeaderRecord(setSkipHeaderRecord)
                 .get();
+    }
+
+    private List<User> getUsersFromFile() throws IOException {
+        try (Reader input = new FileReader("SpringBootAppRepeat/src/main/resources/users.csv")) {
+
+            CSVFormat csvFormat = getCSVFormat(true );
+
+            try (CSVParser parser = csvFormat.parse(input)) {
+
+                List<User> users = parser.stream()
+                        .map(record -> {
+                            int id = Integer.parseInt(record.get("id"));
+                            String name = record.get("name");
+                            int age = Integer.parseInt(record.get("age"));
+                            boolean isMale = Boolean.parseBoolean(record.get("isMale"));
+                            return new User(id, name, age, isMale);
+                        })
+                        .sorted(Comparator.comparingInt(User::id))
+                        .toList();
+                return users;
+            }
+        }
     }
 }
