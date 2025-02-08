b50178f7902ac058e371fe5ca6b467b6decdafd8
lukaszjargielo <l.jargielo88@gmail.com>
2025-02-08 17:16:40 +0100

    POST method with JPA

--- Spring-web-jpa_/src/main/java/pl/futurejava/Spring_web_jpa/UserController.java
+++ Spring-web-jpa_/src/main/java/pl/futurejava/Spring_web_jpa/UserController.java
@@ -3,8 +3,12 @@ package pl.futurejava.Spring_web_jpa;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Contract;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,16 @@ public class UserController {
         Iterable<User> users = userRepository.findAll();
 
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("users")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(user);
     }
 }
