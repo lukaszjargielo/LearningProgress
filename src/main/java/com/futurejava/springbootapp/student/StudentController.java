9c18903de8172c98539df85461e37b797d22c80f
lukaszjargielo <l.jargielo88@gmail.com>
2025-02-27 12:35:00 +0100

    Implementing dao layer

--- src/main/java/com/futurejava/springbootapp/student/StudentController.java
+++ src/main/java/com/futurejava/springbootapp/student/StudentController.java
@@ -1,8 +1,6 @@
 package com.futurejava.springbootapp.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,14 +9,34 @@ import java.util.List;
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
-    private StudentService studentService;
+    private StudentService service;
 
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Student save(@RequestBody Student student) {
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(@PathVariable String email) {
+        return service.findByEmail(email);
     }
 
     @GetMapping()
     public List<Student> findAllStudents() {
-        return studentService.findAllStudents();
+        return service.findAllStudents();
+    }
+
+    @PutMapping
+    public Student updateStudent(@RequestBody Student student) {
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void deleteByEmail(@PathVariable("email") String email) {
+        service.delete(email);
     }
 }
--- src/main/java/com/futurejava/springbootapp/student/StudentService.java
+++ src/main/java/com/futurejava/springbootapp/student/StudentService.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
 
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student("Łukasz", "Jargieło", LocalDate.of(1988, 3, 4), "lukasz.jargielo1988@gmail.com", 36),
-                new Student("Natalia", "Jargieło", LocalDate.of(1989, 3, 26), "natalia.grzegorzyca@gmail.com", 35),
-                new Student("Alicja", "Jargieło", LocalDate.of(1995, 6, 19), "alicjajar@gmail.com", 29)
-        );
-    }
+public interface StudentService {
+
+    Student save(Student s);
+
+    List<Student> findAllStudents();
+
+    Student findByEmail(String email);
+
+    Student update(Student s);
+
+    void delete(String email);
 }
